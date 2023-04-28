8a0d15203478efadef2b873b77a087425aab211e
ShreemBrzee06 <makwanapunammk@gmail.com>
2023-04-28 22:26:54 +0100

    test friday done

---
+++ src/Smiley.java
@@ -0,0 +1,9 @@
+public class Smiley {
+    public static void main (String[]args){
+
+        System.out.println("     o     o      ");
+        System.out.println("   *    v    *    ");
+        System.out.println("      *    *       ");
+
+    }
+}
